88c073454eaf77979596cb3f833e881bedb86921
Pushp Gupta <114074001+Steller12@users.noreply.github.com>
2025-06-18 00:51:02 +0530

    Time: 0 ms (100%), Space: 26 MB (9.38%) - LeetHub

--- 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements.py
+++ 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements.py
@@ -1,36 +1,17 @@
-MOD = 10**9 + 7
-MX = 10**5
+mx = 10**5 + 10
+mod = 10**9 + 7
+f = [1] + [0] * mx
+g = [1] + [0] * mx
 
-fact = [0] * MX
-inv_fact = [0] * MX
-
-
-def qpow(x, n):
-    res = 1
-    while n:
-        if n & 1:
-            res = res * x % MOD
-        x = x * x % MOD
-        n >>= 1
-    return res
+for i in range(1, mx):
+    f[i] = f[i - 1] * i % mod
+    g[i] = pow(f[i], mod - 2, mod)
 
 
-def init():
-    if fact[0] != 0:
-        return
-    fact[0] = 1
-    for i in range(1, MX):
-        fact[i] = fact[i - 1] * i % MOD
-    inv_fact[MX - 1] = qpow(fact[MX - 1], MOD - 2)
-    for i in range(MX - 1, 0, -1):
-        inv_fact[i - 1] = inv_fact[i] * i % MOD
-
-
-def comb(n, m):
-    return fact[n] * inv_fact[m] % MOD * inv_fact[n - m] % MOD
+def comb(m: int, n: int) -> int:
+    return f[m] * g[n] * g[m - n] % mod
 
 
 class Solution:
     def countGoodArrays(self, n: int, m: int, k: int) -> int:
-        init()
-        return comb(n - 1, k) * m % MOD * qpow(m - 1, n - k - 1) % MOD
+        return comb(n - 1, k) * m * pow(m - 1, n - k - 1, mod) % mod
