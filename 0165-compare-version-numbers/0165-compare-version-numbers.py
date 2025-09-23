dd7383e951bc4c3cbccb752a89c7ad478309959d
Pushp Gupta <114074001+Steller12@users.noreply.github.com>
2025-09-24 00:45:20 +0530

    Time: 0 ms (100%), Space: 12.4 MB (83.25%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def compareVersion(self, version1, version2):
+        """
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+        v1=list(map(int, version1.split(".")))
+        v2=list(map(int, version2.split(".")))
+        l=max(len(v1),len(v2))
+        for i in range(l):
+            n1=v1[i] if i < len(v1) else 0
+            n2=v2[i] if i < len(v2) else 0
+            if n1<n2 :
+                return -1
+            elif n2<n1:
+                return 1
+        return 0
