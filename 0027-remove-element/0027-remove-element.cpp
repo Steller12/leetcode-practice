9f65af402360f0158beee275c665b50a81cf5d85
Pushp Gupta <114074001+Steller12@users.noreply.github.com>
2025-10-03 05:10:15 +0530

    Time: 0 ms (100%), Space: 11.7 MB (81.5%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int index=0;
+        int k=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]!=val){
-                nums[index]=nums[i];
-                index++;
+            if (nums[i]==val){
+                nums[i]=55;
+                k++;
             }
         }
-        return index;
+        k=nums.size()-k;
+        sort(nums.begin(),nums.end());
+        return k;
     }
 };
